3a77c5e1b94f1e9c11df9474c57542ed02a215b1
LetRicette <leticiaricette@id.uff.br>
2021-12-27 23:36:01 -0300

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.grey),
+        title: Image.asset(
+          'images/youtube.png',
+          width: 98,
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.videocam),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.search),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.account_circle),
+          ),
+        ],
+      ),
+    );
+  }
+}
